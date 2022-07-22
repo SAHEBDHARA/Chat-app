92cffdc203ac9e0e42f1fd6b6df021125a0c9439
Saheb Dhara <sahebdhara961@gmail.com>
2022-07-22 13:46:35 +0530

    d

---
+++ ms Chat/sudo/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+
+import './App.css'
+
+const App = ()=>{
+return(
+  <ChatEngine 
+  height = "100vh"
+  projectID = "1d396cb6-cf2c-4c33-a23a-d415468a4313"
+  userName = "nimaida"
+  userSecret = "12345"
+  />
+)
+}
+
+export default App;
